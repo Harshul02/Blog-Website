27172bece2b0fa6d1e6264d74352e070d81e1944
Harshul Jain <harshulgangwal02@gmail.com>
2022-09-17 12:43:48 +0530

    Added compose page

--- app.js
+++ app.js
@@ -25,7 +25,13 @@ app.get("/about", function(req, res){
 
 app.get("/contact", function(req, res){
   res.render("contact", {aboutContact : contactContent});
+});
+
+
+app.get("/compose", function(req, res){
+  res.render("compose");
 })
+
 
 
 
